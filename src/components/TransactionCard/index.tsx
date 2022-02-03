9ff50744fcc63515bce529acffab55a2c4dac2e1
joaogabrielramos28 <joaogabrielramos28@outlook.com>
2022-02-03 19:43:38 -0300

    Finishing list of transactions + fix bug transaction type

--- src/components/TransactionCard/index.tsx
+++ src/components/TransactionCard/index.tsx
@@ -1,5 +1,5 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { categories } from "../../utils/categories";
 import {
   Container,
   Title,
@@ -11,16 +11,11 @@ import {
   Date,
 } from "./styles";
 
-interface CategoryProps {
-  name: string;
-  icon: string;
-}
-
 export interface TransactionCardProps {
   type: "positive" | "negative";
-  title: string;
+  name: string;
   amount: string;
-  category: CategoryProps;
+  category: string;
   date: string;
 }
 
@@ -29,9 +24,12 @@ interface Props {
 }
 
 export function TransactionCard({ data }: Props) {
+  const category = categories.find((item) => item.key === data.category);
+  console.log(data?.type);
+
   return (
     <Container>
-      <Title>{data.title}</Title>
+      <Title>{data.name}</Title>
 
       <Amount type={data.type}>
         {data.type === "negative" && "- "}
@@ -40,8 +38,8 @@ export function TransactionCard({ data }: Props) {
 
       <Footer>
         <Category>
-          <Icon name={data.category.icon} />
-          <CategoryName>{data.category.name}</CategoryName>
+          <Icon name={category?.icon} />
+          <CategoryName>{category?.name}</CategoryName>
         </Category>
         <Date>{data.date}</Date>
       </Footer>
