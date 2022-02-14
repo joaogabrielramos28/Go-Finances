664caa42e4ceaa471d12ee3086e1ee360f525cc8
joaogabrielramos28 <joaogabrielramos28@outlook.com>
2022-02-14 19:48:08 -0300

    Creating signIn screen for social login

--- App.tsx
+++ App.tsx
@@ -8,6 +8,9 @@ import { ThemeProvider } from "styled-components";
 import theme from "./src/global/styles/theme";
 import { NavigationContainer } from "@react-navigation/native";
 import { AppRoutes } from "./src/routes/app.routes";
+
+import { SignIn } from "./src/screens/SignIn";
+
 import {
   useFonts,
   Poppins_400Regular,
@@ -28,7 +31,8 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <NavigationContainer>
         <StatusBar barStyle="light-content" />
-        <AppRoutes />
+        {/* <AppRoutes /> */}
+        <SignIn />
       </NavigationContainer>
     </ThemeProvider>
   );
